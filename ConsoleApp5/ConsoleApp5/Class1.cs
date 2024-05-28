436b25b56f51c5f7337db693634b7449978cb83c
opatel7 <166631646+opatel7@users.noreply.github.com>
2024-05-27 19:17:01 -0600

    sample

---
+++ ConsoleApp5/ConsoleApp5/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Class1
+    {
+        public string name = "Welcome";
+        public int age = 18;
+        public void Displaydata() // methods
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace ConsoleApp5
+{ 
+    class Car
+    {
+        public string Name = "Mustang";
+    }
+    class Program
+    {
+        // Data Members
+        // member functions
+
+        string myvariable = "nothing"; 
+        static void Main(string[] args) // private by default
+        {
+              // object creation 
+            Class1 obj1 = new Class1();
+            Class1 obj2 = new Class1();
+            
+            obj1.Displaydata();
+            obj2.Displaydata();
+
+            Console.ReadLine();
+
+            Car object1 = new Car();
+            Console.WriteLine(object1.Name);
+            Console.ReadLine();
+        }
+    }
+}
